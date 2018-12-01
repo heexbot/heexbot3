2ac8cc5291663a2d18672dcde18372e4335e884b
heexbot <39099888+heexbot@users.noreply.github.com>
2018-12-01 12:49:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,87 +1,45 @@
    const Discord = require('discord.js');
 const client = new Discord.Client();
-
+var prefix = "F"
  
+client.on("message", message => {
 
-
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+            if (message.content.startsWith(prefix + "bc")) {
+        if(message.channel.type === 'dm') return message.reply('This Command Is Only For Server!');
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+}
 });
 
 
 
 
 
-
-
-
-
-client.on('message', message => {
-    var prefix = "F";
-    
-        if (message.author.id === client.user.id) return;
-        if (message.guild) {
-       let embed = new Discord.RichEmbed()
-        let args = message.content.split(' ').slice(1).join(' ');
-    if(message.content.split(' ')[0] == prefix + 'bc1') {
-        if (!args[1]) {
-    message.channel.send("**f!bc <message>**");
-    return;
-    }
-            message.guild.members.forEach(m => {
-       if(!message.member.hasPermission('ADMINISTRATOR')) return;
-                var bc = new Discord.RichEmbed()
-                .addField('» السيرفر :', `${message.guild.name}`)
-                .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
-                .addField(' » الرسالة : ', args)
-                .setColor('RANDOM')
-                // m.send(`[${m}]`);
-                m.send(`${m}`,{embed: bc});
-            });
-        }
-        } else {
-            return;
-        }
+  client.on('message', async message => {
+  if(message.content.startsWith(prefix + "رسالة")) {
+    let i = client.users.size;
+    if(message.author.id !== '323888904602124299') return message.channel.send('❎ » هذا الأمر مخصص لصاحب البوت فقط');
+    var args = message.content.split(' ').slice(1).join(' ');
+    if(!args) return message.channel.send('❎ » يجب عليك كتابة الرسالة')
+    setTimeout(() => {
+      message.channel.send(`تم الارسال لـ ${i} شخص`)
+    }, client.users.size * 500);
+    client.users.forEach(s => {
+      s.send(args).catch(e => i--);
     });
-	
-	
-	
-
-
-
-
-
-
-
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`skran`);
-        console.log(`---------------`);
-      console.log(`ON ${client.guilds.size} Servers `);
-    console.log(`---------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-  
+  }
 });
 
-
-
-client.on('message', message => {
-    let args = message.content.split(' ').slice(1).join(' ');
-    if (message.content.startsWith('Fbc2')){ // البريفكس والامر
-    if(!message.author.id === '') return;
-    message.channel.sendMessage('جار ارسال الرسالة :white_check_mark:')
-    client.users.forEach(m =>{
-    m.sendMessage(args)
-    })
-    }
-    });
-
-
-
-
+__
+كود برودكاست مدموج مع كود bcall
+من صنعي طبعا
+@here l @everyone 
 
 
 
